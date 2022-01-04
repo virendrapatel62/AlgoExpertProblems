944290237f6dd0e6bb5960070090a11811ca04f0
Virendra Kumar Patel <virendra.patel@dowjones.com>
2022-01-04 21:00:19 +0530

    validate sub sequence

--- 2.ValidateSubsequence/index.js
+++ 2.ValidateSubsequence/index.js
@@ -1,5 +1,6 @@
 const array = [5, 1, 22, 25, 6, -1, 8, 10];
-const subSequence = [22 , 25 , -1];
+const subSequence = [5 , 25, -1];
+
 
 function isValidSubsequence1(array = [], sequence = []) {
   let nextStart = 0;
@@ -29,5 +30,19 @@ function isValidSubsequence2(array = [], sequence = []) {
     return sequenceIndex === sequence.length
 }
 
+function isValidSubsequence3(array = [] , subSequence = []){
+  let arrayIndex = 0  , sequenceIndex = 0 
+  while(arrayIndex < array.length && sequenceIndex < subSequence.length){
+    if(subSequence[sequenceIndex] === array[arrayIndex]){
+      sequenceIndex ++ 
+    }
+    arrayIndex ++
+  }
+  return sequenceIndex === subSequence.length 
+}
+
+
 console.log(isValidSubsequence1(array, subSequence));
 console.log(isValidSubsequence2(array, subSequence));
+console.log(isValidSubsequence3(array, subSequence));
+
